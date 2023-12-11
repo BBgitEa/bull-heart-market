a9fa08b5a3a8cf02d2367ef09e42971537cf5e3b
Artemiy Trefilov <trefilovartem10@gmail.com>
2023-12-11 20:04:53 +0400

    add category model, view, template

--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
-
+from .models import Category
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello there!")
+    categories = Category.objects.all()
+    count = categories.count()
+    return render(request, "index.html", {'categories': categories, 'count': count})
