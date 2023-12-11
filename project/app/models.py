4d84b739fd1584d76afa6048e6a6253f9bbb3b13
Artemiy Trefilov <trefilovartem10@gmail.com>
2023-12-11 20:34:04 +0400

    добавил модель товара

--- project/app/models.py
+++ project/app/models.py
@@ -4,4 +4,18 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    preview = models.ImageField(upload_to ='uploads/previews/')
+    preview = models.ImageField(upload_to ='uploads/previews/', blank=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, blank=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, default="No description")
+    price = models.FloatField(blank=True, default=0)
+    wiki_link = models.TextField(blank=True, default="No wiki link")
+
+    def __str__(self):
+        return self.name
