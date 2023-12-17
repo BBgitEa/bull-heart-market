65c824e4d397e277eaefbf1fe0a37902392725ea
Artemiy Trefilov <trefilovartem10@gmail.com>
2023-12-17 19:32:23 +0400

    add cart and productstack models

--- project/app/models.py
+++ project/app/models.py
@@ -28,3 +28,10 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+class Cart(models.Model):
+    user = models.OneToOneField(User, related_name='cart', on_delete=models.CASCADE) 
+
+class ProductStack(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_DEFAULT, default=0)
+    count = models.IntegerField(blank=False, default=1)
